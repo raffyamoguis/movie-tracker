26a86687e4ffbe7215a468683a53b54583a96e4d
unknown <raffyamoguis@gmail.com>
2022-10-16 17:26:45 +0800

    setup dropdown

--- src/components/AppTabs.js
+++ src/components/AppTabs.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/apptabs.css'
-import { Nav, NavDropdown, Row, Col } from 'react-bootstrap'
+import { Nav, NavDropdown, Row, Col, Dropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Outlet } from 'react-router-dom'
+import axios from 'axios';
 
 import { genres } from '../data/data'
 
+const getDropwdownApiURL = 'https://myflixer-video-api.cyclic.app/genres';
+
 const AppTabs = () => {
+    const [genres, setGenres] = useState([]);
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                axios.get(`${getDropwdownApiURL}?type=2`)
+                    .then(function (response) {
+                        setGenres(response.data);
+                    });
+            } catch (error) {
+                console.log(error)
+            }
+
+            try {
+                axios.get(`${getDropwdownApiURL}?type=3`)
+                    .then(function (response) {
+                        setCountries(response.data);
+                    });
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetch();
+    }, []);
+
+    const fetchData = (apiurl) => {
+
+    }
+
     return (
         <>
             <Nav
@@ -23,19 +57,23 @@ const AppTabs = () => {
                         GENRE
                     </Nav.Link>
                 </LinkContainer> */}
-                <NavDropdown title="GENRE" id="nav-dropdown" className='tab-link'>
-                    <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-                    {/* <NavDropdown.Divider /> */}
-                    <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+                <NavDropdown title="GENRE" id="nav-dropdown-menu" className='tab-link'>
+                    <Row className='ps-3' md={4}>
+                        {
+                            genres.map((genre) => (
+                                <Col>
+                                    <NavDropdown.Item eventKey="4.2">{genre.name}</NavDropdown.Item>
+                                </Col>
+                            ))
+                        }
+                    </Row>
                 </NavDropdown>
                 {/* <LinkContainer to='/country'>
                     <Nav.Link className='tab-link' href="/country">
                         COUNTRY
                     </Nav.Link>
                 </LinkContainer> */}
-                <NavDropdown title="COUNTRY" id="nav-dropdown" className='tab-link'>
+                <NavDropdown title="COUNTRY" id="nav-dropdown-menu" className='tab-link'>
                     {/* <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
                     <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
@@ -43,9 +81,9 @@ const AppTabs = () => {
                     <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item> */}
                     <Row className='ps-3' md={4}>
                         {
-                            genres.map((genre) => (
+                            countries.map((country) => (
                                 <Col>
-                                    <NavDropdown.Item eventKey="4.2">{genre}</NavDropdown.Item>
+                                    <NavDropdown.Item eventKey="4.2">{country.name}</NavDropdown.Item>
                                 </Col>
                             ))
                         }
