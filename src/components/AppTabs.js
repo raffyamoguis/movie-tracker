8d16b98763c1fede8ab46116e982228568638a17
unknown <raffyamoguis@gmail.com>
2022-10-21 11:38:23 +0800

    added genre

--- src/components/AppTabs.js
+++ src/components/AppTabs.js
@@ -1,28 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import '../css/apptabs.css'
-import { Nav, NavDropdown, Row, Col, Dropdown } from 'react-bootstrap'
+import { Nav, NavDropdown, Row, Col } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Outlet } from 'react-router-dom'
 import axios from 'axios';
 
-import { genres } from '../data/data'
-
 const getDropwdownApiURL = 'https://myflixer-video-api.cyclic.app/genres';
 
 const AppTabs = () => {
-    const [genres, setGenres] = useState([]);
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         const fetch = async () => {
-            try {
-                axios.get(`${getDropwdownApiURL}?type=2`)
-                    .then(function (response) {
-                        setGenres(response.data);
-                    });
-            } catch (error) {
-                console.log(error)
-            }
 
             try {
                 axios.get(`${getDropwdownApiURL}?type=3`)
@@ -37,10 +26,6 @@ const AppTabs = () => {
         fetch();
     }, []);
 
-    const fetchData = (apiurl) => {
-
-    }
-
     return (
         <>
             <Nav
@@ -52,22 +37,11 @@ const AppTabs = () => {
                 <LinkContainer to='/tvshows'>
                     <Nav.Link className='tab-link' href="/tvshows">TV SHOWS</Nav.Link>
                 </LinkContainer>
-                {/* <LinkContainer to='/genre'>
+                <LinkContainer to='/genre'>
                     <Nav.Link className='tab-link' href="/genre">
                         GENRE
                     </Nav.Link>
-                </LinkContainer> */}
-                <NavDropdown title="GENRE" id="nav-dropdown-menu" className='tab-link'>
-                    <Row className='ps-3' md={4}>
-                        {
-                            genres.map((genre) => (
-                                <Col>
-                                    <NavDropdown.Item eventKey="4.2">{genre.name}</NavDropdown.Item>
-                                </Col>
-                            ))
-                        }
-                    </Row>
-                </NavDropdown>
+                </LinkContainer>
                 {/* <LinkContainer to='/country'>
                     <Nav.Link className='tab-link' href="/country">
                         COUNTRY
--- src/pages/GenreContent.js
+++ src/pages/GenreContent.js
@@ -1,19 +1,88 @@
-import React from 'react'
-import { ListGroup, Button, Form } from 'react-bootstrap';
-import { genres } from '../data/data'
+import React, { useEffect, useState, useRef } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import MovieBox from '../components/MovieBox';
+import LoadingBar from 'react-top-loading-bar'
+import axios from 'axios';
+
+const getDropwdownApiURL = 'https://myflixer-video-api.cyclic.app/genres?type=2';
+const getMovieGenreApiURL = 'https://myflixer-video-api.cyclic.app/genre/';
 
 const GenreContent = () => {
+    const [genres, setGenres] = useState([]);
+    const [genrecontent, setGenreContent] = useState([]);
+    const [selgenre, setSelGenre] = useState('action');
+    const [isComplete, setComplete] = useState(false);
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        try {
+            axios.get(getDropwdownApiURL)
+                .then(function (response) {
+                    setGenres(response.data);
+                });
+        } catch (error) {
+            console.log(error)
+        }
+    }, []);
+
+    useEffect(() => {
+        ref.current.continuousStart();
+        const fetchSelGenre = async () => {
+            try {
+                axios.get(`${getMovieGenreApiURL + selgenre}`)
+                    .then((response) => {
+                        setGenreContent(response.data.data)
+                        ref.current.complete();
+                        setComplete(true);
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchSelGenre();
+    }, [selgenre])
+
     return (
-        <div className='ms-3'>
-            <Form.Select aria-label="Select Genre" className='w-25 m-2'>
-                <option>Open this select menu</option>
-                {
-                    genres.map((genre) => (
-                        <option value={genre}>{genre}</option>
-                    ))
-                }
-            </Form.Select>
-        </div>
+        <>
+            <LoadingBar color="#faae2c" ref={ref} shadow={true} height={3} />
+            {isComplete && <div className='ms-3'>
+                <div style={{ display: 'inline-block' }}>
+                    {
+                        genres.map((genre) => (
+                            <a
+                                className='btn btn-sm btn-light mt-1 me-1'
+                                onClick={e => setSelGenre(genre.src)}
+                                href
+                            >
+                                {genre.name}
+                            </a>
+                        ))
+                    }
+                </div>
+
+                <div>
+                    <h3 className='mt-4' style={{ textTransform: 'capitalize' }}>{selgenre}</h3>
+                    <Row md={8} className='g-0'>
+                        {genrecontent.map((moviedata) => {
+                            return (
+                                <Col className='mt-2'>
+                                    <MovieBox
+                                        title={moviedata.name}
+                                        banner={moviedata.poster}
+                                        year={moviedata.year}
+                                        duration={moviedata.duration}
+                                        type={moviedata.type}
+                                        link={moviedata.link}
+                                    />
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </div>
+            </div >
+            }
+        </>
     )
 }
 
